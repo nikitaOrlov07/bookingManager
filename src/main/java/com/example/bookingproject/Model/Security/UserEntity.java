d7763bd45d6b8b40e0eb5d0be5c28790918b6d8f
Nikita Orlov <nikitaorlov145@gmail.com>
2024-07-24 00:44:29 +0300

    Add kubernates files for project deployment

--- src/main/java/com/example/bookingproject/Model/Security/UserEntity.java
+++ src/main/java/com/example/bookingproject/Model/Security/UserEntity.java
@@ -18,7 +18,7 @@ import java.util.*;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
